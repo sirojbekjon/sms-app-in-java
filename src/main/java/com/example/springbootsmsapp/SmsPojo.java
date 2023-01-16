993021fb31c1a41e9b2ca6b97d559f115fc9c86b
sirojsms1712@gmail.com <sirojsms1712@gmail.com>
2023-01-16 17:57:57 +0500

    sms-app use from Twilio

---
+++ src/main/java/com/example/springbootsmsapp/SmsPojo.java
@@ -0,0 +1,23 @@
+package com.example.springbootsmsapp;
+
+public class SmsPojo {
+    private String to;
+    private String message;
+
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
